8a8fe3c2e0c3eee19432c0db40ea6bfe50313783
unknown <landzbej@gmail.com>
2023-06-12 17:35:57 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import Editor from "@monaco-editor/react"
+import * as Y from "yjs"
+import { WebrtcProvider } from "y-webrtc"
+import { MonacoBinding } from "y-monaco"
+
+
+function App() {
+  const editorRef = useRef(null);
+
+  const handleEditorDidMount = (editor, monaco) => {
+    editorRef.current = editor;
+
+    const doc = new Y.Doc();
+    const provider = new WebrtcProvider("test-room", doc);
+    const type = doc.getText("monaco");
+    const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness)
+    console.log("provider awareness", provider.awareness)
+  }
+
+  return (
+    <Editor 
+    height="100vh"
+    width="100vw"
+    theme="vs-dark"
+    onMount={handleEditorDidMount}
+    />
+  )
+}
+
+export default App
