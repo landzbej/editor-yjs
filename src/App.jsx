52acfa8ebb013e19a5c3209ff3bf15410c5065a8
unknown <landzbej@gmail.com>
2023-06-18 14:08:20 -0400

    try for vps version

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     const doc = new Y.Doc();
 
     // const provider = new WebsocketProvider('ws://localhost:1234', 'new-room', doc)
-    const wsProvider = new WebsocketProvider('wss://localhost:1234', 'my-roomname', doc)
+    const provider = new WebsocketProvider('wss://localhost:1234', 'my-roomname', doc)
         // const provider = new WebrtcProvider("test-room", doc)
     const type = doc.getText("monaco");
     const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness)
