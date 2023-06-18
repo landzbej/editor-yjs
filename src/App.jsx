65d44b2c79b684f4c28ea568d8cf2063dafcbaf2
unknown <landzbej@gmail.com>
2023-06-18 14:25:53 -0400

    http not wss

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     const doc = new Y.Doc();
 
     // const provider = new WebsocketProvider('ws://localhost:1234', 'new-room', doc)
-    const provider = new WebsocketProvider('wss://server.landzbergs.com', 'my-roomname', doc)
+    const provider = new WebsocketProvider('https://server.landzbergs.com', 'my-roomname', doc)
         // const provider = new WebrtcProvider("test-room", doc)
     const type = doc.getText("monaco");
     const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness)
